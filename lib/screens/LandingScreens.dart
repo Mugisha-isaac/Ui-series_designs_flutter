a8111a15bc00da8e94b6003262bc1627aad84a1c
Mugisha-isaac <mugishaisaac2001@gmail>
2022-03-13 22:42:40 +0200

    feat(BorderScreen,-images,-constants,-custom_functions,-sample_data,-widget_function): setting up env to speed up development
    this will fast dev because i shall only call definde classes

---
+++ lib/screens/LandingScreens.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class LandingScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: Container(
+        width: size.width,
+        height: size.height,
+        child: Column(),
+      ),
+    );
+  }
+}
