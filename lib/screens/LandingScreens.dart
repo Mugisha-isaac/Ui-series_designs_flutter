ec5346cb9a128eddc6627bc55c09a7e694099952
Mugisha-isaac <mugishaisaac2001@gmail>
2022-03-16 22:49:25 +0200

    fix(LandingScreen.dart): almost finishing up the design on LandingScreen

--- lib/screens/LandingScreens.dart
+++ lib/screens/LandingScreens.dart
@@ -1,15 +1,174 @@
+// ignore_for_file: use_key_in_widget_constructors
+
 import 'package:flutter/material.dart';
+import 'package:uidesign/custom/BorderIcon.dart';
+import 'package:uidesign/utils/widget_functions.dart';
+
+import '../utils/constants.dart';
+import '../utils/custom_functions.dart';
+import '../utils/sample_data.dart';
 
 class LandingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
+    final ThemeData themeData = Theme.of(context);
+    const double padding = 25;
+    final sidePadding = EdgeInsets.symmetric(horizontal: padding);
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          width: size.width,
+          height: size.height,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              addVerticalSpace(padding),
+              Padding(
+                padding: sidePadding,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    BorderIcon(
+                      child: Icon(
+                        Icons.menu,
+                        color: COLOR_BLACK,
+                      ),
+                      height: 50,
+                      width: 50,
+                      padding: EdgeInsets.all(5),
+                    ),
+                    BorderIcon(
+                      child: Icon(
+                        Icons.settings,
+                        color: COLOR_BLACK,
+                      ),
+                      height: 50,
+                      width: 50,
+                      padding: EdgeInsets.all(5),
+                    )
+                  ],
+                ),
+              ),
+              addVerticalSpace(padding),
+              Padding(
+                padding: sidePadding,
+                child: Text(
+                  "City",
+                  style: themeData.textTheme.bodyText2,
+                ),
+              ),
+              addVerticalSpace(10),
+              Padding(
+                padding: sidePadding,
+                child:
+                    Text("San Fransisco", style: themeData.textTheme.headline1),
+              ),
+              Padding(
+                padding: sidePadding,
+                child: const Divider(
+                  height: padding,
+                  color: COLOR_GREY,
+                ),
+              ),
+              addVerticalSpace(10),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: ["<\$220,000", "For Sale", "3-4 Beds", ">1000 sqft"]
+                      .map((filter) => ChoiceOption(text: filter))
+                      .toList(),
+                ),
+              ),
+              addVerticalSpace(10),
+              Expanded(
+                child: Padding(
+                  padding: sidePadding,
+                  child: ListView.builder(
+                      itemCount: RE_DATA.length,
+                      itemBuilder: (context, index) {
+                        return RealEstateItem(itemData: RE_DATA[index]);
+                      }),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
 
-    return Scaffold(
-      body: Container(
-        width: size.width,
-        height: size.height,
-        child: Column(),
+class ChoiceOption extends StatelessWidget {
+  final String text;
+  const ChoiceOption({required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData themeData = Theme.of(context);
+    return Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20.0),
+          color: COLOR_GREY.withAlpha(25)),
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 13),
+      margin: const EdgeInsets.only(left: 25),
+      child: Text(
+        text,
+        style: themeData.textTheme.headline5,
+      ),
+    );
+  }
+}
+
+class RealEstateItem extends StatelessWidget {
+  final dynamic itemData;
+
+  const RealEstateItem({Key? key, @required this.itemData}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData themeData = Theme.of(context);
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Stack(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(25),
+                child: Image.asset(itemData["image"]),
+              ),
+              const Positioned(
+                  top: 15,
+                  right: 15,
+                  child: BorderIcon(
+                      child: Icon(Icons.favorite_border, color: COLOR_BLACK),
+                      padding: EdgeInsets.all(5),
+                      width: 50,
+                      height: 50)),
+            ],
+          ),
+          addVerticalSpace(15),
+          Row(
+            children: [
+              Text(
+                "${formatCurrency(itemData["amount"])}",
+                style: themeData.textTheme.headline1,
+              ),
+              addHorizontalSpace(10),
+              Text(
+                "${itemData["address"]}",
+                style: themeData.textTheme.bodyText2,
+              ),
+              addVerticalSpace(10),
+              Text(
+                "${itemData["bedrooms"]} bedrooms / ${itemData["bathrooms"]} bathrooms / ${itemData["area"]} sqft",
+                style: themeData.textTheme.headline5,
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
